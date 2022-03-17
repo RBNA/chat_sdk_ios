0491d4cb7006bcd0f792f58dcaef043367eae850
Freddy Hernandez Jr <freddy.hernandez@redbull.com>
2022-03-17 10:59:38 -0700

    changed name of binary and path

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZendeskChatSDK",
-            path: "ChatSDK.xcframework"
+            path: "ZendeskChatSDK.xcframework"
         ),
         .target(name: "ZendeskChatSDKTargets",
                 dependencies: [
